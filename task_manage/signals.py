b2ffb7ada49c9c80d78b5cac1ab40be802e6cdd3
sreejith <sreejith.thekkumkoottathil@gmail.com>
2021-01-03 23:04:33 +0530

    updates in email notification

--- task_manage/signals.py
+++ task_manage/signals.py
@@ -7,13 +7,14 @@ from core.models import Task
 
 @receiver(post_save, sender=Task)
 def notify_admin(sender, instance, created, **kwargs):
-    subject = f"Task Updated - {instance.name} by {instance.user.name}"
-    message = f"Task updated by {instance.user.name} \n"
+    subject = f"Task Updated - {instance.name}"
+    message = f"Kindly find the task details below:   \n"
     if created:
         subject = f"Task Created - {instance.name} by {instance.user.name}"
         message = f"New task created by {instance.user.name} \n"
 
     message_detail = f"{message}TasK Name: {instance.name}\n" \
                      f"Description: {instance.description}\n" \
-                     f"Time Required: {instance.time_required}"
+                     f"Time Required: {instance.time_required}\n" \
+                     f"Created By: {instance.user.name}"
     send_email(subject=subject, message=message_detail)
